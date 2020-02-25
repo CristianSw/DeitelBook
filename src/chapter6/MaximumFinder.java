92332d5ecedc82e40755a869539e16b6578b42bc
CristianSw <cristian.sw@protonmail.com>
2020-02-25 19:21:50 +0200

    solving idea issues

---
+++ src/chapter6/MaximumFinder.java
@@ -0,0 +1,31 @@
+package practiceFromDeitel.chapter6;
+
+import java.util.Scanner;
+
+public class MaximumFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double first, second, third;
+        double result;
+
+        System.out.println("Enter 3 numbers: ");
+        first = scanner.nextDouble();
+        second = scanner.nextDouble();
+        third = scanner.nextDouble();
+
+        result = maximum(first, second, third);
+
+        System.out.println("Result is : " + result);
+
+    }
+
+    public static double maximum(double first, double second, double third) {
+        double localMax;
+        localMax = Math.max(first, second);
+        if (localMax > third) {
+            return localMax;
+        } else {
+            return third;
+        }
+    }
+}

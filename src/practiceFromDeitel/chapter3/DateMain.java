2ff6c82de73afce87cbcc32983eeddc9d85e5d36
CristianSw <cristian.sw@protonmail.com>
2020-02-25 19:14:40 +0200

    start project from zero

--- src/practiceFromDeitel/chapter3/DateMain.java
+++
@@ -1,8 +0,0 @@
-package practiceFromDeitel.chapter3;
-//Ex. 3.14
-public class DateMain {
-    public static void main(String[] args) {
-        Date date1 = new Date(12,2,2001);
-        date1.displayDate();
-    }
-}

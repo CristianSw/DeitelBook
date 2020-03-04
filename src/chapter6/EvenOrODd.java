987a2709211d382310090a1d96077b236a316977
CristianSw <cristian.sw@protonmail.com>
2020-03-04 06:26:56 +0200

    EvenOrOdd clas added and method isEven was provided

---
+++ src/chapter6/EvenOrODd.java
@@ -0,0 +1,23 @@
+package chapter6;
+
+import java.util.Scanner;
+
+public class EvenOrODd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+
+        System.out.println("Enter please the number: ");
+        while (scanner.hasNext()){
+            number = scanner.nextInt();
+            System.out.println("Is Even : " + isEven(number));
+        }
+    }
+
+    public static boolean isEven(final int number) {
+        if (number % 2 == 0)
+            return true;
+        else
+            return false;
+    }
+}
